5eee7e88f2091d130e0f9a50add76b5727170e24
DoPhuc22 <phucsonmai999@gmail.com>
2024-01-31 08:38:01 +0700

    admin

--- source_code/laravel_perfume/app/Providers/AppServiceProvider.php
+++ source_code/laravel_perfume/app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,7 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //paginator bootstrap 5
-        Paginator::useBootstrapFive();
+        //
     }
 }
---
+++ source_code/laravel_perfume/resources/views/layouts/navbar_adminMenu.blade.php
@@ -0,0 +1,60 @@
+
+<nav class="d-flex flex-column flex-shrink-0 p-3 text-white rounded-3 navbar-expand-sm bg-dark" style="height: 100%; background-color: #03002e" >
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-light text-decoration-none">
+<!--        <span class="bi me-2 me-4">-->
+<!--            <img src="../resources/images/brand.png" width="50" height="40">-->
+<!--        </span>-->
+        <span class="fs-3 ml-3">
+            Perfume Store
+        </span>
+    </a>
+    <hr style="background-color: white">
+    <ul class="nav nav-pills flex-column height-100 fs-4 gap-2 p-1 small rounded-5 shadow-sm">
+        <span>
+            Menu
+        </span>
+        <li class="nav-item link-opacity-50-hover">
+            <button href="#" class="fs-3 nav-link link-light link-offset-1-hover bi bi-house mx-3 mt-3 ">
+                <span class="ms-3">Dashboard</span>
+            </button>
+        </li>
+        <li class="nav-item link-opacity-50-hover">
+            <a href="#" class="fs-3 nav-link link-light bi bi-bar-chart mx-3">
+                <span class="ms-3">Statistics</span>
+            </a>
+        </li>
+        <span class="my-sm-3">
+            Manage
+        </span>
+        <li class="nav-item link-opacity-50-hover">
+            <a href="#" class="fs-3 nav-link link-light bi bi-box2 mx-3">
+                <span class="ms-3">Products</span>
+            </a>
+        </li>
+        <li class="nav-item link-opacity-50-hover">
+            <a href="#" class="fs-3 nav-link link-light bi bi-file-earmark-richtext mx-3">
+                <span class="ms-3">Orders</span>
+            </a>
+        </li>
+        <li class="nav-item link-opacity-50-hover">
+            <a class="fs-3 nav-link link-light bi bi-person mx-3">
+                <span class="ms-3">Customers</span>
+            </a>
+        </li>
+        <span class="my-sm-3">
+            Other
+        </span>
+        <li class="nav-item link-opacity-50-hover">
+            <a href="#" class="fs-3 nav-link link-light bi bi-gear mx-3">
+                <span class="ms-3">Settings</span>
+            </a>
+        </li>
+        <li class="nav-item link-opacity-50-hover">
+            <a href="../login_logout/login.php" class="fs-3 nav-link link-light bi bi-box-arrow-in-left mx-3">
+                <span class="ms-3">Sign out</span>
+            </a>
+        </li>
+    </ul>
+    <hr>
+
+</nav>
