b89ed5dd7729f820c84e8558758710e404f2cac1
TranNguyenChiAn <117379845+TranNguyenChiAn@users.noreply.github.com>
2024-02-19 11:29:14 +0700

    Update cart
    Hiển thị cart khi thêm vào giỏ hàng và nhấn vào cart_icon

--- source_code/laravel_perfume/resources/views/layouts/nav.blade.php
+++ source_code/laravel_perfume/resources/views/layouts/nav.blade.php
@@ -48,11 +48,162 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('product.cart') }}">
-                        <i class="bi bi-cart"></i>
+                    <a data-bs-toggle="offcanvas" href="#offcanvasExample" style="color: #c0c0c0">
+                        <i class="bi bi-cart" style="padding-top: 30px"></i>
                     </a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
+
+<!--   CART -->>
+<div class="offcanvas offcanvas-end" id="offcanvasExample"
+     tabindex="-1" aria-modal="true" aria-labelledby="offcanvasExampleLabel">
+    <!-- Close -->
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close">
+        <i class="fe fe-x" aria-hidden="true"></i>
+    </button>
+
+    <!-- Header-->
+    <div class="offcanvas-header lh-fixed fs-lg">
+        <strong class="mx-auto">Your Cart </strong>
+    </div>
+
+    <!-- Body-->
+    <ul class="list-group list-group-lg lh-fixed list-group-flush">
+        <li class="list-group-item">
+            @if(\Illuminate\Support\Facades\Session::get('cart') != null)
+                @foreach(Session::get('cart') as $product_id => $product)
+                    <form class="m-0" action="{{route('product.updateCartQuantity', $product_id)}}">
+                        <div class="row align-items-center">
+
+                            <div class="col-4">
+                                <!-- Image -->
+                                <a href="/product/{{$product_id}}"
+                                   class="overflow-hidden ratio ratio-1x1 d-flex align-items-center justify-content-center">
+                                    <img src="{{asset($product['image'])}}" class="object-fit-cover">
+                                </a>
+
+                            </div>
+                            <div class="col-8">
+                                <!-- Title -->
+                                <p class="fs-sm fw-bold mb-6">
+                                    <a class="text-body">{{$product['product_name']}}</a> <br>
+                                    <span class="text-muted">${{$product['price']}}</span>
+                                </p>
+
+
+                                <!--Footer -->
+                                <div class="d-flex align-items-center">
+                                    <!-- Quantity -->
+                                    <input type="number" name="buy_quantity" min="1"
+                                           value="{{$product['quantity']}}"
+                                           class="form-control rounded-3"
+                                           style="margin-right: 20px"
+                                           onchange="this.form.submit()">
+                                    @php
+                                        $total_items[$product_id] = $product['quantity']
+                                    @endphp
+
+                                        <!-- Select -->
+                                    <select class="form-select form-select-xxs w-auto" style="margin-right: 20px">
+                                        <option value="3">50ml</option>
+                                        <option value="4">100ml</option>
+                                        <option value="5">125ml</option>
+                                        <option value="6">200ml</option>
+                                    </select>
+
+                                    <!-- Remove -->
+                                    <a class="fs-xs text-gray-400 ms-auto" href="{{route('product.deleteFromCart', $product_id)}}">
+                                        <i style="color:red" class="bi bi-x"></i>
+                                    </a>
+
+                                </div>
+                            </div>
+
+                            <div class="w-10 p-2 text-success">
+                                ${{$product['price'] * $product['quantity']}}
+                                @php
+                                    $amount[$product_id] = $product['price'] * $product['quantity']
+                                @endphp
+                            </div>
+                        </div>
+                    </form>
+                @endforeach
+            @else
+                 <div class="d-flex align-items-center justify-content-center flex-column min-vh-50">
+                     <div class="fs-6 mb-3">
+                          Your cart is empty
+                     </div>
+                          <a href="{{route('product')}}" class="btn btn-primary rounded-5">Shop now</a>
+                 </div>
+            @endif
+        </li>
+    </ul>
+
+    <!-- Footer -->
+    <div class="offcanvas-footer justify-between lh-fixed fs-sm bg-light mt-auto">
+        <div class="mb-3 d-flex justify-content-around">
+            <div> Total items </div>
+            <div>
+                @if(\Illuminate\Support\Facades\Session::get('cart') != null)
+                    {{array_sum($total_items)}}
+                @else
+                    0
+                @endif
+            </div>
+        </div>
+        <div class="mb-3 d-flex justify-content-around">
+                    <div>
+                        Amount
+                    </div>
+                    <div class="text-success">
+                        @if(\Illuminate\Support\Facades\Session::get('cart') != null)
+                            ${{array_sum($amount)}}
+                        @else
+                            $0
+                        @endif
+                    </div>
+                </div>
+        <div class="d-flex align-items-center justify-content-center">
+                <a class="btn w-70 btn-dark" href="#">
+                    Continue to checkout
+                </a>
+            </div>
+        <div class="d-flex align-items-center justify-content-center" style="margin-top: 10px; margin-bottom: 10px">
+            <a class="btn w-70 btn-outline-dark" href="{{route('product')}}">
+                Continue shopping
+            </a>
+        </div>
+    </div>
+
+    <!-- Empty cart (remove `.d-none` to enable it) -->
+    <div class="d-none">
+
+        <!-- Close -->
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close">
+            <i class="fe fe-x" aria-hidden="true"></i>
+        </button>
+
+        <!-- Header-->
+        <div class="offcanvas-header lh-fixed fs-lg">
+            <strong class="mx-auto">Your Cart (0)</strong>
+        </div>
+
+        <!-- Body -->
+        <div class="offcanvas-body flex-grow-0 my-auto">
+
+            <!-- Heading -->
+            <h6 class="mb-7 text-center">Your cart is empty 😞</h6>
+
+            <!-- Button -->
+            <a class="btn w-100 btn-outline-dark" href="/product">
+                Continue Shopping
+            </a>
+
+        </div>
+
+    </div>
+
+</div>
